d4778f510470e72469dbab79bee3112e324951ec
ZcvGuilherme <169081282+ZcvGuilherme@users.noreply.github.com>
2024-09-25 15:53:21 -0300

    commit

--- src/Terminal/Menu.java
+++ src/Terminal/Menu.java
@@ -3,7 +3,12 @@ package Terminal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
+/*
+será que nao está codigo demais?
+mais de 100 linhas, um monte de funções fazendo coisas diferentes, será que nao da pra separar e otimizar um pouco?
+cade as pastas separadas que eu fiz?
+elas servem exatamente pra separar os arquivos que estao juntas aqui
+ */
 public class Menu {
     private List<Partido> partidos;
     private Scanner scanner;
@@ -16,6 +21,9 @@ public class Menu {
     private void limpaTela() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
+        /*
+         * muito bom, mas está acontecendo um problema durante o codigo, que é q eu nao consigo ver a ultima msg, como a de que foi cadastrado...
+         */
     }
 
     private void cadastrarPresidente(Partido partido) {
@@ -25,13 +33,17 @@ public class Menu {
         System.out.print("Digite o número do presidente: ");
         int numeroPresidente = scanner.nextInt();
         scanner.nextLine(); // Limpa o buffer
-
+        /*
+         * esse if... se vc so consegue cadastrar um presidente por vez, qual a necessidade dele?
+         * outra coisa, uma coisa que deveria ser gerenciada: e se eu colocar o mesmo numero do anterior?
+         */
         Candidato presidente = new Candidato(nomePresidente, partido.getNome(), numeroPresidente, "presidente");
         if (partido.adicionarCandidato(presidente)) {
             System.out.println("Presidente cadastrado com sucesso!");
         } else {
             System.out.println("Erro ao cadastrar presidente. Já existe um candidato para este cargo.");
         }
+
     }
 
     private void cadastrarGovernador(Partido partido) {
@@ -49,7 +61,18 @@ public class Menu {
             System.out.println("Erro ao cadastrar governador. Já existe um candidato para este cargo.");
         }
     }
-
+    /*
+     * outra sugestao:
+     * voce ve q teve que fazer o mesmo método para governador e presidente, correto?
+     * e eu estou pendindo ali embaixo pra tu fazer o mesmo para o senador e deputados
+     * se ta tendo repetição, vc pode fazer esse loop, com as mesmas verificações que eu coloquei
+     * la embaixo, em um metodo generico, que vai criar um candidato generico pro loop, q vc vai 
+     * ter que passar pelos atributos as mudanças, por exemplo, o que muda?
+     * o nome do cargo
+     * a verificação de quantos candidatos daquele cargo podem ter
+     * 
+     * sao so dois atributos, que se vc fizer uma classe generica vc consegue armazenar com mais facilidade, isso aproveita e diminui o codigo separando ele mais em outros arquivos
+     */
     private void cadastrarCandidato(Partido partido) {
         while (true) {
             limpaTela();
@@ -104,6 +127,21 @@ public class Menu {
         cadastrarGovernador(partido);
         
         // Cadastro de senadores e deputados
+        /*
+         essa parte.... pq vc n fez um para cada?
+         ficaria mais organizado e menos confuso, do mesmo jeito que voce fez para o presidente e  governador, deve ter para o resto dos candidatos, inclusive está faltando a tratação de erros, a minha sugestão é que voce refaça a parte desse dos candidatos, porque ele tem q ter a seguinte lógica:
+         senadores:
+         entra na função/loop dos senadores:
+         pergunta quantos são:
+         pede o nome e o número exato que o candidato precisa ter
+         e o loop continua até que o numero de senador seja atingido
+
+
+         a mesma coisa pros deputados
+
+         da mais trabalho mas fica mais facil nao so pra colocar dps mas tbm pra automatizar o processo também
+
+         */
         cadastrarCandidato(partido);
     }
 
