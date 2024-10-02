c5a8c5a75dd7994316a12ff3cfa56a9b642a633c
1sad0ra <isadora.cd13@gmail.com>
2024-10-02 16:09:01 -0300

    novo
    novo

---
+++ src/GUI/Frame_Urna/CriadorComponentes.java
@@ -0,0 +1,21 @@
+package GUI.Frame_Urna;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CriadorComponentes {
+
+    public static JLabel criarLabel(String texto, int x, int y, int largura, int altura) {
+        JLabel label = new JLabel(texto);
+        label.setBounds(x, y, largura, altura);
+        label.setFont(new Font("Arial", Font.PLAIN, 50)); // Define a fonte
+        return label;
+    }
+
+    public static JTextField criarTextField(int x, int y, int largura, int altura) {
+        JTextField textField = new JTextField();
+        textField.setBounds(x, y, largura, altura);
+        textField.setFont(new Font("Arial", Font.PLAIN, 30));
+        return textField;
+    }
+}
