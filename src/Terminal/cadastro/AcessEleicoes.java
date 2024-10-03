ad71d662b401e44647831ade13607034e7106948
ZcvGuilherme <169081282+ZcvGuilherme@users.noreply.github.com>
2024-10-03 20:50:52 -0300

    .

--- src/Terminal/cadastro/AcessEleicoes.java
+++ src/Terminal/cadastro/AcessEleicoes.java
@@ -3,7 +3,6 @@ import DATABASE.DAO.UserDAO;
 import DATABASE.entidade.Candidato;
 import DATABASE.entidade.Eleicao;
 import Terminal.utili.utilitaveis;
-import java.io.PrintWriter;
 import java.util.List;
 
 public class AcessEleicoes {
@@ -38,19 +37,5 @@ public class AcessEleicoes {
         List<Candidato> candidatos = dao.retornoTodosCandidatos();
         int tempo = retornarTempo(id);
         Eleicao objEleicao = new Eleicao(candidatos, id, tempo, 0, 0, 0, 0, 0, 0);
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-        MainServidor.iniciarServidor(objEleicao);
-=======
-
-        
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        out.println("Eleição ID " + id + " iniciada com sucesso! Duração: " + tempo + " minutos.");
-       // System.out.println("Eleição iniciada com ID: " + id + " e duração de " + tempo + " minutos."); 
-       
->>>>>>> Stashed changes
-=======
-
->>>>>>> parent of ecbfade (commit conexao)
     }
 }
